f2e5913358de3cde337d2ac8381acff82e3f25ce
152017017 <josuasirait25@gmail.com>
2022-09-17 14:34:42 +0700

    ...

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -15,6 +15,8 @@
         </li>
       </ul>
 
+      @can('admin')
+
       <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
         <span>Administrator</span>
       </h6>
@@ -26,6 +28,8 @@
           </a>
         </li>
       </ul>
+
+      @endcan
 
     </div>
   </nav>
